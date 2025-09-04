8920431968cac431fb70b46e736062b67dff2fd0
tibhafidon-blip <tibhafidon@gmail.com>
2025-09-04 14:04:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;vIEzjZKb#CpU1OMujIqt9j8AKKFERq2Opf1SyF1IFkVXxIvopGMc", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;mRkAnIKR#LlKZpFkHxiORaFycr_GDVAgdIJK0eaksaTGnAqtDEGw", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
